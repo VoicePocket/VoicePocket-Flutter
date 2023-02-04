02d4f87901678c32a526e1c9dbbba714120d1466
HuMolee <jjj990407@naver.com>
2023-02-04 15:51:17 +0900

    change play screen

--- lib/screens/voicepocket_play_screen.dart
+++ lib/screens/voicepocket_play_screen.dart
@@ -1,7 +1,4 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:voicepocket/constants/gaps.dart';
 import 'package:voicepocket/constants/sizes.dart';
 import 'package:voicepocket/screens/create_model.dart';
 import 'package:carousel_slider/carousel_slider.dart';
@@ -32,13 +29,13 @@ class _VoicePocketPlayScreenState extends State<VoicePocketPlayScreen> {
   double _value = 0.0;
 
   final List<String> images = [
-    'images/playwidget.png',
-    'images/playwidget.png',
-    'images/playwidget.png',
-    'images/playwidget.png',
-    'images/playwidget.png',
-    'images/playwidget.png',
-    'images/playwidget.png',
+    'assets/images/playpage.png',
+    'assets/images/playpage.png',
+    'assets/images/playpage.png',
+    'assets/images/playpage.png',
+    'assets/images/playpage.png',
+    'assets/images/playpage.png',
+    'assets/images/playpage.png',
   ];
 
   final List<String> places = [
@@ -54,7 +51,7 @@ class _VoicePocketPlayScreenState extends State<VoicePocketPlayScreen> {
     return images.map((element)=>ClipRRect(
       borderRadius: BorderRadius.circular(15.0),
       child: Image.asset(element,
-        fit:BoxFit.contain,
+        fit:BoxFit.none,
       ),
     )).toList();
   }
@@ -76,124 +73,131 @@ class _VoicePocketPlayScreenState extends State<VoicePocketPlayScreen> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Center(
+      body: Column(
+        children: [
+        Center(
           child: Padding(
             padding: const EdgeInsets.symmetric(
               vertical: Sizes.size40,
               horizontal: Sizes.size16,
             ),
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                Container(
-                  padding: const EdgeInsets.only(top: 50),
-                  child: Stack(
-                    alignment: Alignment.topCenter,
-                    children: [
-                      CarouselSlider(items: generateImagesTiles(), options: CarouselOptions(
-                        enlargeCenterPage: true,
-                      )),
-                    ],
+                Flexible( //Carousel slider 크기 조절 부분
+                  //fit: FlexFit.tight,
+                  //flex:6,
+                  child: Container(                     
+                    alignment: Alignment.topCenter,             
+                    //padding: const EdgeInsets.only(top: 50),
+                    child: Stack(
+                      alignment: Alignment.topCenter,
+                      children: [
+                        CarouselSlider(items: generateImagesTiles(), options: CarouselOptions(
+                          enlargeCenterPage: true,
+                        )),
+                      ],
+                    ),
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    IconButton(
-                      padding: EdgeInsets.all(20),
-                      onPressed: ()=> MessageEvent(""),
-                      icon: Image.asset(
-                        "assets/images/speaker.png",
-                        width: 40,
-                        height: 40,
-                      ),
+                Flexible( //재생관련 부분 크기 조절 
+                  //fit: FlexFit.tight,
+                  //flex: 3,
+                  child: Container(                            
+                    alignment: Alignment.bottomCenter,      
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            IconButton(
+                              padding: EdgeInsets.all(20),
+                              onPressed: ()=> (""),
+                              icon: Image.asset(
+                                "assets/images/speaker.png",
+                                width: 40,
+                                height: 40,
+                              ),
+                            ),
+                            IconButton(
+                              padding: EdgeInsets.all(20),
+                              onPressed: ()=> (""),
+                              icon: Image.asset(
+                                "assets/images/playlist.png",
+                                width: 40,
+                                height: 40,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Slider(
+                          activeColor: Theme.of(context).primaryColor,
+                            value: _value,
+                            onChanged: (value) {
+                              setState(() {
+                                _value=value;
+                              });
+                            }
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            IconButton(
+                              padding: EdgeInsets.all(20),
+                              onPressed: ()=> (""),
+                              icon: Image.asset(
+                                "assets/images/return.png",
+                                width: 40,
+                                height: 40,
+                              ),
+                            ),
+                            IconButton(
+                              padding: EdgeInsets.all(20),
+                              onPressed: ()=> (""),
+                              icon: Image.asset(
+                                "assets/images/back-button.png",
+                                width: 40,
+                                height: 40,
+                              ),
+                            ),
+                            IconButton(
+                              padding: EdgeInsets.all(20),
+                              onPressed: ()=> (""),
+                              icon: Image.asset(
+                                "assets/images/play-button-arrowhead.png",
+                                width: 40,
+                                height: 40,
+                              ),
+                            ),
+                            IconButton(
+                              padding: EdgeInsets.all(20),
+                              onPressed: ()=> (""),
+                              icon: Image.asset(
+                                "assets/images/forward-button.png",
+                                width: 40,
+                                height: 40,
+                              ),
+                            ),
+                            IconButton(
+                              padding: EdgeInsets.all(20),
+                              onPressed: ()=> (""),
+                              icon: Image.asset(
+                                "assets/images/random.png",
+                                width: 40,
+                                height: 40,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ]
                     ),
-                    IconButton(
-                      padding: EdgeInsets.all(20),
-                      onPressed: ()=> MessageEvent(""),
-                      icon: Image.asset(
-                        "assets/images/playlist.png",
-                        width: 40,
-                        height: 40,
-                      ),
-                    ),
-                  ],
-                ),
-                Slider(
-                  activeColor: Theme.of(context).primaryColor,
-                    value: _value,
-                    onChanged: (value) {
-                      setState(() {
-                        _value=value;
-                      });
-                    }
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    IconButton(
-                      padding: EdgeInsets.all(20),
-                      onPressed: ()=> MessageEvent(""),
-                      icon: Image.asset(
-                        "assets/images/return.png",
-                        width: 40,
-                        height: 40,
-                      ),
-                    ),
-                    IconButton(
-                      padding: EdgeInsets.all(20),
-                      onPressed: ()=> MessageEvent(""),
-                      icon: Image.asset(
-                        "assets/images/back-button.png",
-                        width: 40,
-                        height: 40,
-                      ),
-                    ),
-                    IconButton(
-                      padding: EdgeInsets.all(20),
-                      onPressed: ()=> MessageEvent(""),
-                      icon: Image.asset(
-                        "assets/images/play-button-arrowhead.png",
-                        width: 40,
-                        height: 40,
-                      ),
-                    ),
-                    IconButton(
-                      padding: EdgeInsets.all(20),
-                      onPressed: ()=> MessageEvent(""),
-                      icon: Image.asset(
-                        "assets/images/forward-button.png",
-                        width: 40,
-                        height: 40,
-                      ),
-                    ),
-                    IconButton(
-                      padding: EdgeInsets.all(20),
-                      onPressed: ()=> MessageEvent(""),
-                      icon: Image.asset(
-                        "assets/images/random.png",
-                        width: 40,
-                        height: 40,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ],
             ),
           ),
         ),
-      ),
-      floatingActionButton: Align(
-        alignment: Alignment.bottomCenter,
-        child: FloatingActionButton(
-          backgroundColor: Theme.of(context).primaryColor,
-          foregroundColor: Colors.white,
-          onPressed: () => _onCreateModelTab(context),
-          child: const Icon(
-            Icons.add_circle_outline_outlined,
-            size: Sizes.size44,
-          ),
-        ),
+        ]
       ),
     );
   }
