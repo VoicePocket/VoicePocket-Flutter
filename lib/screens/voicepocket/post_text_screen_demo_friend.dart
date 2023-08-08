d2d39cba912c057a17715400076437430ae062d3
HuMooole <jjj990407@naver.com>
2023-08-08 22:32:21 +0900

    FIX : 친구 채팅방 로딩 오류 수정

--- lib/screens/voicepocket/post_text_screen_demo_friend.dart
+++ lib/screens/voicepocket/post_text_screen_demo_friend.dart
@@ -1,6 +1,6 @@
 //현재 사용 중인 페이지
-
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:just_audio/just_audio.dart';
@@ -11,7 +11,6 @@ import 'package:voicepocket/services/post_text.dart';
 import 'package:voicepocket/services/token_refresh_post.dart';
 import 'package:voicepocket/models/database_service.dart';
 import 'package:voicepocket/widgets/message_tile.dart';
-import 'package:voicepocket/widgets/message_tile_indicator.dart';
 
 import '../authentications/home_screen.dart';
 
@@ -23,8 +22,7 @@ class PostTextScreenDemoFriend extends StatefulWidget {
   });
 
   @override
-  State<PostTextScreenDemoFriend> createState() =>
-      _PostTextScreenDemoFriendState();
+  State<PostTextScreenDemoFriend> createState() => _PostTextScreenDemoFriendState();
 }
 
 class _PostTextScreenDemoFriendState extends State<PostTextScreenDemoFriend> {
@@ -33,26 +31,29 @@ class _PostTextScreenDemoFriendState extends State<PostTextScreenDemoFriend> {
   TextModel? response;
   String inputText = "";
   String wavUrl = "";
-  bool isLoading = false;
   String defaultEmail = "";
-  late ScrollController _scrollController;
   List<QueryDocumentSnapshot> listMessage = [];
   late AudioPlayer audioPlayer = audioPlayer;
   final BehaviorSubject<PlayerState> _playerStateSubject =
       BehaviorSubject<PlayerState>();
-
-  bool isUILoading = false;
-  bool bottomFlag = true;
-  bool isScrollBottomFixed = true;
+  late ScrollController _scrollController;
 
   @override
   void initState() {
     getChat();
     super.initState();
-    bool isUILoading = false;
-    bool bottomFlag = true;
+
     _scrollController = ScrollController();
-    _scrollController.addListener(_scrollListener);
+
+    // 리스트 뷰의 스크롤 위치가 변경될 때마다 호출되는 리스너를 추가합니다.
+    _scrollController.addListener(() {
+      if (_scrollController.position.atEdge &&
+          _scrollController.position.pixels != 0) {
+        // 리스트 뷰의 맨 아래로 스크롤하려는 경우, 더 많은 메시지를 로드합니다.
+        loadMoreMessages();
+      }
+    });
+
     _textController.addListener(() {
       setState(() {
         inputText = _textController.text;
@@ -69,22 +70,10 @@ class _PostTextScreenDemoFriendState extends State<PostTextScreenDemoFriend> {
     );
   }
 
-  _scrollListener() {
-    if (_scrollController.offset >=
-            _scrollController.position.maxScrollExtent &&
-        !_scrollController.position.outOfRange) {
-      setState(() {
-        isScrollBottomFixed = true;
-      });
-    } else {
-      setState(() {
-        isScrollBottomFixed = false;
-      });
-    }
-  }
-
-  getChat() {
-    DatabaseService().getChats(widget.email).then((val) {
+  getChat() async{
+    final pref = await SharedPreferences.getInstance();
+    defaultEmail = pref.getString("email")!;
+    DatabaseService().getFriendsChats(defaultEmail, widget.email).then((val) {
       setState(() {
         chats = val;
       });
@@ -97,32 +86,10 @@ class _PostTextScreenDemoFriendState extends State<PostTextScreenDemoFriend> {
     final wavUrl = '${widget.email}/$uuid.wav';
     final notiEmail = widget.email;
     defaultEmail = pref.getString("email")!;
-    setState(() {
-      isLoading = true;
-    });
+    setState(() {});
     var response = await postTextDemo(text, widget.email, uuid);
-    /* await Future.delayed(
-      const Duration(
-        seconds: 10,
-      ),
-    );
-    if (!mounted) return ''; */
     if (response.success) {
-      setState(() {
-        isLoading = false;
-      });
-      /* Map<String, dynamic> chatMessageMap = {
-        "message": "https://storage.googleapis.com/voicepocket/$wavUrl",
-        "sender": 'SERVER',
-        "time": DateTime.now().millisecondsSinceEpoch,
-      };
-      if (defaultEmail == notiEmail) {
-        DatabaseService().sendMessage(defaultEmail, chatMessageMap);
-      } else {
-        DatabaseService()
-            .sendMessageForFriend(defaultEmail, notiEmail, chatMessageMap);
-      } */
-      print(wavUrl);
+      setState(() {});
       return wavUrl;
     } else if (response.code == -1006) {
       await tokenRefreshPost();
@@ -177,12 +144,12 @@ class _PostTextScreenDemoFriendState extends State<PostTextScreenDemoFriend> {
                     width: MediaQuery.of(context).size.width,
                     height: MediaQuery.of(context).size.height * 0.1,
                     child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 18),
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
                       height: MediaQuery.of(context).size.height * 0.1,
                       width: MediaQuery.of(context).size.width,
                       color: const Color.fromRGBO(243, 230, 255, 0.816),
                       child: Row(
+                        //mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Expanded(
                               child: TextFormField(
@@ -195,14 +162,13 @@ class _PostTextScreenDemoFriendState extends State<PostTextScreenDemoFriend> {
                               border: InputBorder.none,
                             ),
                           )),
-                          const SizedBox(
-                            width: 12,
-                          ),
+                          /* const SizedBox(
+                        width: 12,
+                      ), */
                           InkWell(
-                            onTap: () {
-                              sendMessageForFriend(inputText);
-                              _postTextTab(inputText);
-                              bottomFlag = true;
+                            onTap: () async {
+                              sendMessage(inputText);
+                              wavUrl = await _postTextTab(inputText);
                             },
                             child: Container(
                               height: 50,
@@ -228,7 +194,36 @@ class _PostTextScreenDemoFriendState extends State<PostTextScreenDemoFriend> {
             )));
   }
 
-  sendMessageForFriend(String text) async {
+  Future<void> loadMoreMessages() async {
+    // 가장 마지막 메시지의 시간을 기준으로 이전 메시지 20개를 가져옴
+    final lastMessageTime = listMessage.isNotEmpty
+        ? listMessage.first['time']
+        : DateTime.now().millisecondsSinceEpoch;
+
+    final moreChats = await getMoreChats(widget.email, lastMessageTime);
+
+    setState(() {
+      listMessage.insertAll(0, moreChats.docs);
+    });
+  }
+
+  Future<QuerySnapshot> getMoreChats(String email, int lastMessageTime) async {
+    try {
+      final querySnapshot = await FirebaseFirestore.instance
+          .collection('chats')
+          .where('sender', isEqualTo: email)
+          .where('time', isLessThan: lastMessageTime)
+          .orderBy('time', descending: true)
+          .limit(20)
+          .get();
+
+      return querySnapshot;
+    } catch (e) {
+      rethrow; // 예외를 다시 던져서 상위 호출자에서 처리하도록 함
+    }
+  }
+
+  sendMessage(String text) async {
     final pref = await SharedPreferences.getInstance();
     defaultEmail = pref.getString("email")!;
     if (text.isNotEmpty) {
@@ -237,8 +232,7 @@ class _PostTextScreenDemoFriendState extends State<PostTextScreenDemoFriend> {
         "sender": defaultEmail,
         "time": DateTime.now().millisecondsSinceEpoch,
       };
-      DatabaseService()
-          .sendMessageForFriend(defaultEmail, widget.email, chatMessageMap);
+      DatabaseService().sendMessageForFriend(defaultEmail, widget.email ,chatMessageMap);
 
       setState(() {
         _textController.clear();
@@ -248,35 +242,39 @@ class _PostTextScreenDemoFriendState extends State<PostTextScreenDemoFriend> {
 
   chatMessages() {
     return SizedBox(
-        height: MediaQuery.of(context).size.height * 0.79,
-        child: StreamBuilder(
-          stream: chats,
-          builder: (context, AsyncSnapshot snapshot) {
-            return snapshot.hasData
-                ? ListView.builder(
-                    reverse: true,
-                    physics: const BouncingScrollPhysics(),
-                    controller: _scrollController,
-                    itemCount: snapshot.data.docs.length,
-                    itemBuilder: (context, index) {
-                      return Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          MessageTile(
-                            message: snapshot.data.docs[index]['message'],
-                            sender: snapshot.data.docs[index]['sender'],
-                            sentByMe: widget.email ==
-                                snapshot.data.docs[index]['sender'],
-                          ),
-                          if (isLoading &&
-                              index == snapshot.data.docs.length - 1)
-                            const MessageTileIndicator(),
-                        ],
-                      );
-                    },
-                  )
-                : Container();
-          },
-        ));
+      height: MediaQuery.of(context).size.height * 0.78,
+      child: StreamBuilder(
+        stream: chats,
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            SchedulerBinding.instance.addPostFrameCallback((_) {
+              // 리스트 뷰가 다시 그려진 후에 아래로 스크롤합니다.
+              _scrollController
+                  .jumpTo(_scrollController.position.maxScrollExtent);
+            });
+            return ListView.builder(
+              controller: _scrollController, // ScrollController를 지정합니다.
+              physics: const BouncingScrollPhysics(),
+              itemCount: snapshot.data.docs.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    MessageTile(
+                      message: snapshot.data.docs[index]['message'],
+                      sender: snapshot.data.docs[index]['sender'],
+                      sentByMe:
+                          defaultEmail == snapshot.data.docs[index]['sender'],
+                    ),
+                  ],
+                );
+              },
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
+    );
   }
 }
--- lib/services/global_var.dart
+++ lib/services/global_var.dart
@@ -5,6 +5,10 @@ class GlobalVariable {
 }
 
 class VoicePocketUri {
-  static const String iosUrl = "http://localhost:8080/api";
-  static const String androidUrl = "http://10.0.2.2:8080/api";
+  //static const String iosUrl = "http://localhost:8080/api";
+  static const String iosUrl = "";
+
+  //ㅌㅌㅌㅌstatic const String androidUrl = "http://10.0.2.2:8080/api";
+  static const String androidUrl = "";
+
 }
