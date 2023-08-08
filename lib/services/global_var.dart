32ec044d555c570614b95ab29c0f823d0a0c8a69
HuMooole <93040438+HuMooole@users.noreply.github.com>
2023-08-08 14:59:22 +0900

    Update global_var.dart

--- lib/services/global_var.dart
+++ lib/services/global_var.dart
@@ -5,6 +5,8 @@ class GlobalVariable {
 }
 
 class VoicePocketUri {
-  static const String iosUrl = "http://localhost:8080/api";
-  static const String androidUrl = "http://10.0.2.2:8080/api";
+  //static const String iosUrl = "http://localhost:8080/api";
+  static const String iosUrl = ""; //실제 AWS주소
+  //static const String androidUrl = "http://10.0.2.2:8080/api";
+   static const String androidUrl = ""; //실제 AWS주소
 }
